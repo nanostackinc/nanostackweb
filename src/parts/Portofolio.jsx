df50547e0a23575e3376a12a8f647523ca10807a
Rafidimas17 <rafidimas1706@gmail.com>
2023-07-26 09:50:04 +0700

    update-testi-porto

---
+++ src/parts/Portofolio.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function Portofolio({data}) {
+  console.log(data.project)
+  return (
+    <section id="portofolio" style={{
+      marginTop: 64
+    }}>
+      <h3 className='title-port'>{data.title}</h3>
+      <div class="card-body">
+        {data
+          .project
+          .map((item) => {
+            const isEvenId = item.id % 2 === 0;
+            const colClassImg = isEvenId
+              ? "col-md-5 order-md-2"
+              : "col-md-5";
+            const colClassText = isEvenId
+              ? "col-md-5 order-md-1"
+              : "col-md-6";
+            return (
+              <div className="card-portofolio row g-0" key={item.id}>
+                <div className={colClassImg}>
+                  <img
+                    src={item.image}
+                    className="car-card img-fluid rounded-start"
+                    alt="image.png"/>
+                </div>
+                <div className={colClassText}>
+                  <div className="text-card  card-body">
+                    <h5 className="card-title">{item.title}</h5>
+                    <p className="card-text">{item.description}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+
+    </section>
+  )
+}
+export default Portofolio
