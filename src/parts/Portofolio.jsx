61f033c3c6223308a4e015e37fe82f95fa6c1f6f
Rafidimas17 <rafidimas1706@gmail.com>
2023-07-27 20:50:55 +0700

    update-responsive-testi-porto-link-navbar

--- src/parts/Portofolio.jsx
+++ src/parts/Portofolio.jsx
@@ -14,20 +14,20 @@ function Portofolio({data}) {
             const isEvenId = item.id % 2 === 0;
             const colClassImg = isEvenId
               ? "col-md-5 order-md-2"
-              : "col-md-5";
+              : "col-md-5 ";
             const colClassText = isEvenId
-              ? "col-md-5 order-md-1"
-              : "col-md-6";
+              ? "col-md-2 order-md-1 "
+              : "col-md-6 ";
             return (
               <div className="card-portofolio row g-0" key={item.id}>
-                <div className={colClassImg}>
+                <div className={colClassImg} id='card-col'>
                   <img
                     src={item.image}
                     className="car-card img-fluid rounded-start"
                     alt="image.png"/>
                 </div>
                 <div className={colClassText}>
-                  <div className="text-card  card-body">
+                  <div className="text-card card-body">
                     <h5 className="card-title mt-4">{item.title}</h5>
                     <p className="card-text">{item.description}</p>
                   </div>
