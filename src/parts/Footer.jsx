38bd9947ffb13106a1738a57c025eace023182e7
Muhammad Dicky Athalla <dickyathallah@gmail.com>
2023-08-07 13:41:32 +0700

    fix footer, contact, wa button

--- src/parts/Footer.jsx
+++ src/parts/Footer.jsx
@@ -16,18 +16,18 @@ const Footer = () => {
             <p>{data.desc}</p>
           </div>
           <div className="col-12 col-md-3">
-            <p><a href="#" className="fw-bolder">Menu</a></p>
+            <p><a href="#" className="fw-700">Menu</a></p>
             <p><a href="#">About Us</a></p>
             <p><a href="#">Portfolio</a></p>
             <p><a href="#">Testimonial</a></p>
             <p><a href="#">Contact</a></p>
           </div>
           <div className="col-12 col-md-3">
-            <p><a href="#" className="fw-bolder">Community</a></p>
+            <p><a href="#" className="fw-700">Community</a></p>
+            <p><a href="https://fastwork.id/user/ivanar6542/web-development-64971353?utm_source=app_sharing" target="_blank">Fastwork</a></p>
             <p><a href="#">Instagram</a></p>
             <p><a href="#">LinkedIn</a></p>
             <p><a href="#">Facebook</a></p>
-            <p><a href="#">Youtube</a></p>
           </div>
         </div>
       </div>
