dab62be8da7772c787e7ef5a53b4f9e90a044aa2
Rafidimas17 <rafidimas1706@gmail.com>
2023-08-28 11:13:19 +0700

    update-porto

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,7 +1,7 @@
 import Logo from './images/icon/Logo_nanostack.webp'
-import Car from './images/img_car.png'
+
 import Click from './images/click.svg'
 import Android from './images/icon/AndroidIcon.svg'
 import Website from './images/icon/WebsiteIcon.png'
 import Product from './images/icon/ProductIcon.svg'
-export {Logo, Car, Android,Website,Product,Click }
+export {Logo,  Android,Website,Product,Click }
