af8beaeedf619a5705932655723a96be834960ae
Rafidimas17 <rafidimas1706@gmail.com>
2023-09-07 00:33:14 +0700

    update pricelist with added localization and fix bug in navbar

--- src/components/Tabs/index.jsx
+++ src/components/Tabs/index.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Tabs=({tabsData,display})=>{
+const Tabs=({tabsData,display,handleActive})=>{
     const[activeTabs,setActiveTabs]=useState(0)
     const handleClick=(index)=>{
         setActiveTabs(index)
+        handleActive(activeTabs)
     }    
+    useEffect(() => {
+        // Inisialisasi nilai activeTabs ke 0 saat komponen pertama kali dimuat
+        setActiveTabs(0);
+    }, []);
     return(
         <div className='container'>
             <div className={`tabs ${display}`}>
